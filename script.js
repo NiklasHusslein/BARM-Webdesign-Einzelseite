d33878642839834b8d39427f597f35edbbc9704e
Niklas Husslein <husslein.niklas@gmail.com>
2024-08-30 08:51:11 +0200

    big update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const playStopButton = document.getElementById("play-stop-button");
   const audioElements = document.querySelectorAll("audio");
-  const muteCheckboxes = document.querySelectorAll(".mute-checkbox");
   let isPlaying = false;
 
   // Initially mute all audio elements
@@ -25,11 +24,95 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     isPlaying = !isPlaying;
   });
-
-  // Mute/Unmute audio based on checkbox state
-  muteCheckboxes.forEach((checkbox, index) => {
-    checkbox.addEventListener("change", () => {
-      audioElements[index].muted = !checkbox.checked;
-    });
-  });
 });
+
+// PONG MINIGAME
+
+padel1Y = padel2Y = 40;
+padelWidth = 10;
+padelHeight = 100;
+ballX = ballY = 50;
+ballDimension = 6;
+velocityX = velocityY = 4;
+score1 = score2 = 0;
+aiSpeed = 2;
+
+window.onload = function () {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  setInterval(update, 1000 / 30);
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowUp") {
+      padel1Y -= 5;
+      event.preventDefault();
+    } else if (event.key === "ArrowDown") {
+      padel1Y += 5;
+      event.preventDefault();
+    }
+  });
+};
+
+function reset() {
+  ballX = canvas.width / 2;
+  ballY = canvas.height / 2;
+  velocityX = -velocityX;
+  velocityY = 4;
+}
+
+function update() {
+  padel1Y = Math.max(0, Math.min(padel1Y, canvas.height - padelHeight));
+
+  ballX += velocityX;
+  ballY += velocityY;
+
+  if (ballY < 0 && velocityY < 0) {
+    velocityY = -velocityY;
+  }
+
+  if (ballY > canvas.height && velocityY > 0) {
+    velocityY = -velocityY;
+  }
+
+  if (ballX > canvas.width) {
+    if (ballY > padel2Y && ballY < padel2Y + padelHeight) {
+      velocityX = -velocityX;
+      deltaY = ballY - (padel2Y + padelHeight / 2);
+      velocityY = deltaY * 0.3;
+    } else {
+      score1++;
+      reset();
+    }
+  }
+
+  if (ballX < 0) {
+    if (ballY > padel1Y && ballY < padel1Y + padelHeight) {
+      velocityX = -velocityX;
+      deltaY = ballY - (padel1Y + padelHeight / 2);
+      velocityY = deltaY * 0.3;
+    } else {
+      score2++;
+      reset();
+    }
+  }
+
+  if (padel2Y + padelHeight / 2 < ballY) {
+    padel2Y += aiSpeed;
+  } else {
+    padel2Y -= aiSpeed;
+  }
+
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, padel1Y, padelWidth, padelHeight);
+  ctx.fillRect(canvas.width - padelWidth, padel2Y, padelWidth, padelHeight);
+  ctx.fillRect(
+    ballX - ballDimension / 2,
+    ballY - ballDimension / 2,
+    ballDimension,
+    ballDimension
+  );
+  ctx.font = "32px Fustat";
+  ctx.fillText(score1, 100, 100);
+  ctx.fillText(score2, canvas.width - 100, 100);
+}
